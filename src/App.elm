7a097c0c1c47abcf3470d08e625c35198e703a71
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-12-28 11:57:28 +0800

    added random event stream

--- src/App.elm
+++ src/App.elm
@@ -2,8 +2,10 @@ module Main exposing (main)
 
 import Html
 import Html.Attributes
+import Random
 import Svg exposing (svg)
 import Svg.Attributes exposing (..)
+import Time exposing (Time)
 
 
 type alias Model =
@@ -17,6 +19,42 @@ type alias Element =
     , cy : Float
     , r : Float
     }
+
+
+type ElementType
+    = Fire
+    | Gold
+    | Wood
+    | Water
+    | Soil
+
+
+initModel : List Element
+initModel =
+    let
+        p =
+            positions
+    in
+        [ Element Fire 100 p.right p.top p.r
+        , Element Wood 100 p.right p.bottom p.r
+        , Element Gold 100 p.left p.top p.r
+        , Element Water 100 p.left p.bottom p.r
+        , Element Soil 100 p.center p.center p.r
+        ]
+
+
+init : ( Model, Cmd msg )
+init =
+    initModel ! []
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div
+        [ Html.Attributes.style [ ( "margin", "8px" ) ] ]
+        [ Html.h1 [] [ Html.text "Five Elements" ]
+        , viewMain model
+        ]
 
 
 positions :
@@ -66,42 +104,6 @@ positions =
         }
 
 
-initModel : List Element
-initModel =
-    let
-        p =
-            positions
-    in
-        [ Element Fire 100 p.right p.top p.r
-        , Element Wood 100 p.right p.bottom p.r
-        , Element Gold 100 p.left p.top p.r
-        , Element Water 100 p.left p.bottom p.r
-        , Element Soil 100 p.center p.center p.r
-        ]
-
-
-init : ( Model, Cmd msg )
-init =
-    initModel ! []
-
-
-view : Model -> Html.Html msg
-view model =
-    Html.div
-        [ Html.Attributes.style [ ( "margin", "8px" ) ] ]
-        [ Html.h1 [] [ Html.text "Five Elements" ]
-        , viewMain model
-        ]
-
-
-type ElementType
-    = Fire
-    | Gold
-    | Wood
-    | Water
-    | Soil
-
-
 colors : List ( ElementType, String )
 colors =
     [ ( Fire, "red" )
@@ -146,7 +148,7 @@ elementColor element =
                     x
 
 
-viewMain : Model -> Html.Html msg
+viewMain : Model -> Html.Html Msg
 viewMain model =
     let
         p =
@@ -168,7 +170,7 @@ viewMain model =
             (List.map viewElement model)
 
 
-viewElement : Element -> Html.Html msg
+viewElement : Element -> Html.Html Msg
 viewElement element =
     let
         color =
@@ -194,17 +196,179 @@ viewElement element =
             ]
 
 
-update : a -> Model -> ( Model, Cmd msg )
+type Msg
+    = Tick Time
+    | Action ( ActionType, ElementType )
+
+
+type ActionType
+    = Attack
+    | Recover
+    | Rest
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model ! []
+    case msg of
+        Tick time ->
+            ( model, generateAction )
+
+        Action ( actionType, elementType ) ->
+            let
+                r =
+                    Debug.log "update action" ( actionType, elementType )
+
+                m =
+                    List.map (\x -> updateElement actionType elementType model x) model
+            in
+                ( m, Cmd.none )
 
 
-subscriptions : a -> Sub msg
+generateAction : Cmd Msg
+generateAction =
+    let
+        gen =
+            Random.pair generateActionType generateElementType
+    in
+        Random.generate Action gen
+
+
+generateActionType : Random.Generator ActionType
+generateActionType =
+    Random.int 1 3
+        |> Random.map
+            (\x ->
+                case x of
+                    1 ->
+                        Attack
+
+                    2 ->
+                        Recover
+
+                    3 ->
+                        Rest
+
+                    _ ->
+                        Debug.crash "Invalid random value"
+            )
+
+
+generateElementType : Random.Generator ElementType
+generateElementType =
+    Random.int 1 5
+        |> Random.map
+            (\x ->
+                case x of
+                    1 ->
+                        Fire
+
+                    2 ->
+                        Water
+
+                    3 ->
+                        Gold
+
+                    4 ->
+                        Wood
+
+                    5 ->
+                        Soil
+
+                    _ ->
+                        Debug.crash "Invalid random value"
+            )
+
+
+updateElement : ActionType -> ElementType -> Model -> Element -> Element
+updateElement actionType elementType model self =
+    if self.elementType == elementType then
+        let
+            downElement =
+                downElementType self.elementType
+                    |> flip lookupElement model
+
+            upElement =
+                upElementType self.elementType
+                    |> flip lookupElement model
+
+            inc =
+                upElement.amount * 0.1
+
+            dec =
+                downElement.amount * 0.1
+
+            newAmount =
+                self.amount + inc - dec
+        in
+            { self | amount = newAmount }
+    else
+        self
+
+
+downElementType : ElementType -> ElementType
+downElementType elementType =
+    case elementType of
+        Water ->
+            Fire
+
+        Fire ->
+            Gold
+
+        Gold ->
+            Wood
+
+        Wood ->
+            Soil
+
+        Soil ->
+            Water
+
+
+upElementType : ElementType -> ElementType
+upElementType elementType =
+    let
+        down1 =
+            downElementType elementType
+
+        down2 =
+            downElementType down1
+    in
+        down2
+
+
+lookupElement : ElementType -> Model -> Element
+lookupElement elementType model =
+    List.filterMap
+        (\x ->
+            if x.elementType == elementType then
+                Just x
+            else
+                Nothing
+        )
+        model
+        |> List.head
+        |> \x ->
+            case x of
+                Nothing ->
+                    Debug.crash
+                        ("ElementType not found:"
+                            ++ (toString
+                                    { elementType = elementType
+                                    , model = model
+                                    }
+                               )
+                        )
+
+                Just x ->
+                    x
+
+
+subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Time.every Time.second Tick
 
 
-main : Program Never Model msg
+main : Program Never Model Msg
 main =
     Html.program
         { init = init
